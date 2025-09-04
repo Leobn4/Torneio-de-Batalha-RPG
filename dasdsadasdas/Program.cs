083e28950c45d36259c396c9acf8acc4b4c37097
EODudu61 <em6817844@gmail.com>
2025-09-03 21:39:51 -0300

    Update Program.cs

--- dasdsadasdas/Program.cs
+++ dasdsadasdas/Program.cs
@@ -12,12 +12,73 @@ public abstract class Criatura
     public void ReceberDano(int dano)
     
 
-    
 
+ public Guerreiro(string nome, int vida, int ataque, int defesa, int EnergiaVigor)
+        : base(nome, vida, ataque, defesa)
+    {
+        EnergiaVigor = energiaVigor;
+    }
+public override void Defesa(Guerreiro)
     
+   
+    public override void Atacar(Criatura alvo)
+    {
+       
+        int chanceCritica = random.Next(1, 101);
+        int danoFinal;
 
+        if (chanceCritica <= 20)
+        {
+            
+            danoFinal = ataque * 2;
+            Console.WriteLine($"O Cavaleiro fez um ataque CRÍTICO em {nome}!");
+        }
+        else
+        {
+          
+            danoFinal = ataque;
+            Console.WriteLine($"O Cavaleiro fez um ataque normal em {nome}.");
+        }
+        
+     
+    }
+     public Arqueiro(string nome, int vida, int ataque, int defesa, int EnergiaVigor)
+        : base(nome, vida, ataque, defesa)
+    {
+        EnergiaVigor = energiaVigor;
+    }
+public override void Defesa(Arqueiro)
+    
+   
+    public override void Atacar(Criatura alvo)
+    {
+       
+        int chanceCritica = random.Next(1, 101);
+        int danoFinal;
+
+        if (chanceErro <= 30)
+        {
+            
+            danoFinal = ataque = 0;
+            Console.WriteLine($" O Arqueiro erro o {nome}!");
+        }
+        else
+        {
+          
+            danoFinal = ataque;
+            Console.WriteLine($"O Arqueiro fez um ataque normal em {nome}.");
+        }
+        
+     
+    }
     
 }
+
+    
+
+    
+
+
 
 
 
