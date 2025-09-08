d319cae1e5def18296f3f9f6af70ca0f484d2a23
Leobn4 <leonardobelem137@gmail.com>
2025-09-08 19:13:35 -0300

    Update Program.cs

--- dasdsadasdas/Program.cs
+++ dasdsadasdas/Program.cs
@@ -1,172 +1,169 @@
 using System;
 using System.Collections.Generic;
 
-public abstract class Criatura 
+public abstract class Criatura
 {
     public string nome;
     public int vida;
     public int ataque;
     public int defesa;
-    
-   
-    public abstract void Atacar(Criatura alvo);
-    
-    public void ReceberDano(int dano)
-        
+
+    public Criatura(string nome, int vida, int ataque, int defesa)
+    {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+    }
+
+    public bool estaVivo()
+    {
+        return vida > 0;
+    }
+
+    public void receberDano(int dano)
+    {
+        int danoFinal = dano - defesa;
+        if (danoFinal < 0)
+            danoFinal = 0;
+
+        vida -= danoFinal;
+        Console.WriteLine($"{nome} tomou {danoFinal} de dano. Vida agora: {vida}");
+    }
+
+    public abstract void atacar(Criatura alvo);
+}
 
 public class Guerreiro : Criatura
 {
- public Guerreiro(string nome, int vida, int ataque, int defesa, int EnergiaVigor)
-        : base(nome, vida, ataque, defesa)
-    {
-        EnergiaVigor = energiaVigor;
-    }
-public override void Defesa(Guerreiro)
-    
-   
-    public override void Atacar(Criatura alvo)
-    {
-       
-        int chanceCritica = random.Next(1, 101);
-        int danoFinal;
+    Random rand = new Random();
 
-        if (chanceCritica <= 20)
+    public Guerreiro(string nome, int vida, int ataque, int defesa) : base(nome, vida, ataque, defesa)
+    {
+    }
+
+    public override void atacar(Criatura alvo)
+    {
+        int chance = rand.Next(1, 101);
+        int dano = ataque;
+
+        if (chance <= 20)
         {
-            
-            danoFinal = ataque * 2;
-            Console.WriteLine($"O Cavaleiro fez um ataque CRÍTICO em {nome}!");
+            dano = dano * 2;
+            Console.WriteLine($"{nome} deu um ataque critico em {alvo.nome}!");
         }
         else
         {
-          
-            danoFinal = ataque;
-            Console.WriteLine($"O Cavaleiro fez um ataque normal em {nome}.");
+            Console.WriteLine($"{nome} atacou {alvo.nome} normal mesmo.");
         }
-        
-     
+
+        alvo.receberDano(dano);
     }
 }
-    public class Arqueiro : Criatura
-    {
-     public Arqueiro(string nome, int vida, int ataque, int defesa, int EnergiaVigor)
-        : base(nome, vida, ataque, defesa)
-    {
-        EnergiaVigor = energiaVigor;
-    }
-public override void Defesa(Arqueiro)
-    
-   
-    public override void Atacar(Criatura alvo)
-    {
-       
-        int chanceCritica = random.Next(1, 101);
-        int danoFinal;
-
-        if (chanceErro <= 30)
-        {
-            
-            danoFinal = ataque = 0;
-            Console.WriteLine($" O Arqueiro erro o {nome}!");
-        }
-        else
-        {
-          
-            danoFinal = ataque;
-            Console.WriteLine($"O Arqueiro fez um ataque normal em {nome}.");
-        }
-        
-     
-    }
-    }
-
 
 public class Mago : Criatura
 {
-    public int mana; 
+    public int mana;
     public int custoMana;
 
-
-    public Mago(string nome, int vida, int ataque, int defesa, int mana, int custoMana)
+    public Mago(string nome, int vida, int ataque, int defesa, int mana, int custoMana) : base(nome, vida, ataque, defesa)
     {
-        nome = nome;
-        vida = vida;
-        ataque = ataque;
-        defesa = defesa;
-        mana = mana;
-        custoMana = custoMana;
+        this.mana = mana;
+        this.custoMana = custoMana;
     }
 
-
-    public override void Atacar(Criatura alvo)
+    public override void atacar(Criatura alvo)
     {
-        if (mana >= custoMana) 
+        if (mana >= custoMana)
         {
-            
-            int danoMagico = ataque; 
-            Console.WriteLine($"{nome} usou ataque mágico em {alvo.nome}!");
-
-
-            
-            alvo.ReceberDano(danoMagico);
-
-        
+            Console.WriteLine($"{nome} jogou um feitiço em {alvo.nome} que ignora a defesa!");
+            alvo.vida -= ataque; // ignora defesa
             mana -= custoMana;
-            Console.WriteLine($"{nome} gastou {custoMana} de mana. Mana restante: {mana}");
+            Console.WriteLine($"{alvo.nome} perdeu {ataque} de vida. Vida dele: {alvo.vida}");
+            Console.WriteLine($"{nome} gastou {custoMana} de mana. Mana que sobra: {mana}");
         }
         else
         {
-    
-            Console.WriteLine($"{nome} não tem mana suficiente para atacar!");
+            Console.WriteLine($"{nome} tentou atacar, mas não tem mana suficiente...");
         }
     }
-    public class Batalha
+}
+
+public class Arqueiro : Criatura
 {
-    private Criatura criaturaA;
-    private Criatura criaturaB;
+    Random rand = new Random();
 
-    public Batalha(Criatura a, Criatura b)
+    public Arqueiro(string nome, int vida, int ataque, int defesa) : base(nome, vida, ataque, defesa)
     {
-        criaturaA = a;
-        criaturaB = b;
     }
 
-    public Criatura Executar()
+    public override void atacar(Criatura alvo)
     {
-        Console.WriteLine($"\n Iniciando batalha entre {criaturaA.nome} e {criaturaB.nome}");
+        int chance = rand.Next(1, 101);
 
-        while (criaturaA.EstaVivo() && criaturaB.EstaVivo())
+        if (chance <= 30)
         {
-            Console.WriteLine($"\n{criaturaA.nome} ataca {criaturaB.nome}");
-            criaturaA.Atacar(criaturaB);
-            if (!criaturaB.EstaVivo()) break;
+            Console.WriteLine($"{nome} errou o ataque em {alvo.nome}... que azar!");
+        }
+        else
+        {
+            Console.WriteLine($"{nome} atirou uma flecha em {alvo.nome}.");
+            alvo.receberDano(ataque);
+        }
+    }
+}
 
-            Console.WriteLine($"{criaturaB.nome} ataca {criaturaA.nome}");
-            criaturaB.Atacar(criaturaA);
+public class Batalha
+{
+    Criatura c1;
+    Criatura c2;
+
+    public Batalha(Criatura c1, Criatura c2)
+    {
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
+    public Criatura executar()
+    {
+        Console.WriteLine($"\nComeçando a batalha: {c1.nome} x {c2.nome}\n");
+
+        while (c1.estaVivo() && c2.estaVivo())
+        {
+            c1.atacar(c2);
+            if (!c2.estaVivo())
+                break;
+
+            c2.atacar(c1);
         }
 
-        Criatura vencedor = criaturaA.EstaVivo() ? criaturaA : criaturaB;
-        Console.WriteLine($"\n Vencedor da batalha: {vencedor.nome}");
+        Criatura vencedor = c1.estaVivo() ? c1 : c2;
+        Console.WriteLine($"\n{vencedor.nome} ganhou a batalha!\n");
+        Console.WriteLine("-------------------------------");
         return vencedor;
     }
 }
 
-
-    public class Torneio
+public class Torneio
 {
-    private List<Criatura> criaturas;
+    List<Criatura> criaturas;
 
     public Torneio(List<Criatura> criaturas)
     {
         if (criaturas.Count < 2)
-            throw new ArgumentException("É necessário pelo menos 2 criaturas para iniciar o torneio.");
+            throw new Exception("Tem que ter pelo menos 2 criaturas pra começar o torneio!");
 
-        criaturas = criaturas;
+        this.criaturas = criaturas;
     }
 
-    public void Iniciar()
+    public void iniciar()
     {
+        Console.WriteLine("=== Torneio começando! ===");
+
+        int rodada = 1;
         while (criaturas.Count > 1)
         {
+            Console.WriteLine($"\nRodada {rodada}:");
             List<Criatura> vencedores = new List<Criatura>();
 
             for (int i = 0; i < criaturas.Count; i += 2)
@@ -176,52 +173,37 @@ public class Mago : Criatura
                     Criatura a = criaturas[i];
                     Criatura b = criaturas[i + 1];
 
-                    Console.WriteLine($"\nBatalha: {a.nome} vs {b.nome}");
-
-                    while (a.EstaVivo() && b.EstaVivo())
-                    {
-                        a.Atacar(b);
-                        if (b.EstaVivo()) b.Atacar(a);
-                    }
-
-                    Criatura vencedor = a.EstaVivo() ? a : b;
-                    Console.WriteLine($"Vencedor: {vencedor.nome}");
+                    Batalha batalha = new Batalha(a, b);
+                    var vencedor = batalha.executar();
                     vencedores.Add(vencedor);
                 }
                 else
                 {
+                    Console.WriteLine($"{criaturas[i].nome} passou direto pra próxima fase.");
                     vencedores.Add(criaturas[i]);
                 }
             }
 
             criaturas = vencedores;
+            rodada++;
         }
 
-        Console.WriteLine($"\n Campeão do Torneio: {criaturas[0].nome}");
+        Console.WriteLine($"\n O campeão do torneio é: {criaturas[0].nome}!!!");
     }
 }
+
 public class Program
 {
     public static void Main()
     {
-        
-        var guerreiro1 = new Guerreiro("Guerreiro A", 100, 30, 10, 50);
-        var guerreiro2 = new Guerreiro("Guerreiro B", 100, 28, 12, 50);
-        var mago = new Mago("Mago", 80, 35, 5, 100, 20);
-        var arqueiro = new Arqueiro("Arqueiro", 90, 25, 8, 40);
+        var guerreiro1 = new Guerreiro("Aragorn, o Herdeiro de Isildur", 100, 30, 10);
+        var guerreiro2 = new Guerreiro("Gimli, o Anão Imbatível", 100, 28, 12);
+        var mago = new Mago("Gandalf, o Cinzento", 80, 35, 5, 100, 20);
+        var arqueiro = new Arqueiro("Legolas, o Mestre das Flechas", 90, 25, 8);
 
-        
         var criaturas = new List<Criatura> { guerreiro1, guerreiro2, mago, arqueiro };
 
-    
         var torneio = new Torneio(criaturas);
-        torneio.Iniciar();
+        torneio.iniciar();
     }
 }
-}
-
-    
-
-
-    
-
